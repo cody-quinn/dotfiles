bf555d0cf377d7b4132d53e2ad9f265b223a6f67
Cody <cody@codyq.dev>
2022-10-08 19:35:15 -0500

    Updated Return YouTube Dislikes

--- system/thonkpad/users/cody/home.nix
+++ system/thonkpad/users/cody/home.nix
@@ -102,8 +102,8 @@
   # qutebrowser config.py and greasemonkey scripts.
   home.file."./.config/qutebrowser/config.py".source = ./config/qutebrowser/config.py;
   home.file."./.config/qutebrowser/greasemonkey/return-youtube-dislike.user.js".source = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/Anarios/return-youtube-dislike/4e0f633a1478191cb9e7fcce085ff7ffda19405e/Extensions/UserScript/Return%20Youtube%20Dislike.user.js";
-    hash = "sha256-RTlB1BRZG2Lvr31LprLVKm6OAJlP2ZKSqiT/Fj2ItZw=";
+    url = "https://raw.githubusercontent.com/Anarios/return-youtube-dislike/de1831f2c74032f8ccfe42a8eb174088363e011f/Extensions/UserScript/Return%20Youtube%20Dislike.user.js";
+    hash = "sha256-aJwGbsZ3XoO/Xf7wGqjWR21XCqbELMBP2SUKE7kwtyw=";
   };
 
   # This value determines the Home Manager release that your
