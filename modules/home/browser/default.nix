4bb27b7c5dd724ba0873a984ef74f1cb097b5f1d
Cody <cody@codyq.dev>
2023-02-26 16:02:42 -0600

    Update 'Return YouTube Dislike' userscript

--- modules/home/browser/default.nix
+++ modules/home/browser/default.nix
@@ -9,8 +9,7 @@
   # qutebrowser config.py and greasemonkey scripts.
   home.file."./.config/qutebrowser/config.py".source = ./config/qutebrowser/config.py;
   home.file."./.config/qutebrowser/greasemonkey/return-youtube-dislike.user.js".source = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/Anarios/return-youtube-dislike/de1831f2c74032f8ccfe42a8eb174088363e011f/Extensions/UserScript/Return%20Youtube%20Dislike.user.js";
-    hash = "sha256-aJwGbsZ3XoO/Xf7wGqjWR21XCqbELMBP2SUKE7kwtyw=";
+    url = "https://raw.githubusercontent.com/Anarios/return-youtube-dislike/6d441a293286c7708e692bb4a76730387a93c578/Extensions/UserScript/Return%20Youtube%20Dislike.user.js";
+    hash = "sha256-7gCakbOMZyJUlcg6D2cpouqVjRKIRVPlmD30BeOgNLU=";
   };
-
 }
