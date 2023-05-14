c53b7f5a65ce8f01d6f23ca78d1bda984595be93
Cody <cody@codyq.dev>
2023-05-13 23:43:43 -0500

    Installed Chromium & updated return YouTube dislikes

--- modules/home/browser/default.nix
+++ modules/home/browser/default.nix
@@ -9,7 +9,7 @@
   # qutebrowser config.py and greasemonkey scripts.
   home.file."./.config/qutebrowser/config.py".source = ./config/qutebrowser/config.py;
   home.file."./.config/qutebrowser/greasemonkey/return-youtube-dislike.user.js".source = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/Anarios/return-youtube-dislike/6d441a293286c7708e692bb4a76730387a93c578/Extensions/UserScript/Return%20Youtube%20Dislike.user.js";
-    hash = "sha256-7gCakbOMZyJUlcg6D2cpouqVjRKIRVPlmD30BeOgNLU=";
+    url = "https://raw.githubusercontent.com/Anarios/return-youtube-dislike/5c73825aadb81b6bf16cd5dff2b81a88562b6634/Extensions/UserScript/Return%20Youtube%20Dislike.user.js";
+    hash = "sha256-5JC3vrPj+kJq68AFtEWwriyCc7sD8nIpqc6dLbjPGso=";
   };
 }
